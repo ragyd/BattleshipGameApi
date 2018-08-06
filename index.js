ab7f57588089c4b8e19a05ab693fb6b7a8fcb12c
Sandy <sfgcusi@gmail.com>
2018-08-05 21:54:37 -0400

    Update init Sequelize

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
+const cors = require('cors')
+const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const Game = require('./src/game.js')
 
-app.get('/game',(req, res) => {
+app.get('/game',(req, res, next) => {
   Game.join(req.query.token)
   .then(game => {
       res.send(game)
@@ -14,7 +17,7 @@ app.get('/game',(req, res) => {
   .catch(error => console.error(error))
 })
 
-app.post('/game', (req, res) => {
+app.post('/game', (req, res, next) => {
   Game.create(req.body)
     .then(game => {
       res.send(game)
@@ -25,20 +28,3 @@ app.post('/game', (req, res) => {
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 })
-
-
-/*
-  POST /game
-  {
-    "rows":
-    "columns"
-  }
-
-  {
-    "gameId": "http://localhost:3000/game?token=asdfasdfw23",
-    "playerId": "asdfasdfasdf"
-  }
-*/
-
-//folders: game-service
-//files: GameService.js
