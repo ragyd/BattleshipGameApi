4c32eb66650bac2e9d43b9db5a513e460b03222a
Sandy Garcia <sfgcusi@gmail.com>
2018-08-10 17:36:47 -0400

    Update Connection.js

--- database/Connection.js
+++ database/Connection.js
@@ -1,14 +1,14 @@
 const Sequelize = require('sequelize'),
-	connection = new Sequelize('Battleship', 'sa', 'Admin2018', {
-		host: 'localhost',
-		dialect: 'mssql',
-		pool: {
-			max: 5,
-			min: 0,
-			acquire: 30000,
-			idle: 10000
-		},
-		storage: './data.mssql'
-	});
+  connection = new Sequelize('Battleship', 'sa', 'Admin2018', {
+    host: 'localhost',
+    dialect: 'mssql',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    storage: './data.mssql'
+  });
 
 module.exports = connection;
