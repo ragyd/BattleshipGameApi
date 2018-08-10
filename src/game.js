0a92e4c7c7b2c6ac54fab7f18538bed844466b5c
Sandy Garcia <sfgcusi@gmail.com>
2018-08-10 17:37:38 -0400

    Update game.js

--- src/game.js
+++ src/game.js
@@ -2,43 +2,43 @@ const idHelper = require('./IdHelper.js')
 const GameBd = require('.././database/GameModel.js')
 
 class Game {
-	constructor({cols = 10, rows = 10} = {}) {
-		this.cols = cols;
-		this.rows = rows;
-	}
+  constructor({cols = 10, rows = 10} = {}) {
+    this.cols = cols;
+    this.rows = rows;
+  }
 
-	static create({cols = 10, rows = 10} = {}) {
-		const game = new Game({cols,rows});
-		game.playerId = idHelper();
-		const token = idHelper();
-		game.token = token
-		game.session = `http://localhost:3000/game?token=${token}`;
-		return GameBd.sync()
-			.then(() => GameBd.create({
-				token,
-				playerId1 : game.playerId
-			}))
-	}
+  static create({cols = 10, rows = 10} = {}) {
+    const game = new Game({cols,rows});
+    game.playerId = idHelper();
+    const token = idHelper();
+    game.token = token
+    game.session = `http://localhost:3000/game?token=${token}`;
+    return GameBd.sync()
+      .then(() => GameBd.create({
+        token,
+        playerId1 : game.playerId
+      }))
+  }
 
-	static join(token) {
-		const game = GameBd.findAll(
-		{ 
-			where: { 
-				token: token 
-			}
-		})
-		if(game === undefined) {
-			return Promise.reject()
-		}
-		GameBd.update({ 
-				playerId2: idHelper() 
-			}, { 
-				where: { 
-					token: token 
-				}
-			})
-		return Promise.resolve(game);
-	}
+  static join(token) {
+    const game = GameBd.findAll(
+    { 
+      where: { 
+        token: token 
+      }
+    })
+    if(game === undefined) {
+      return Promise.reject()
+    }
+    GameBd.update({ 
+        playerId2: idHelper() 
+      }, { 
+        where: { 
+          token: token 
+        }
+      })
+    return Promise.resolve(game);
+  }
 }
 
 module.exports = Game
